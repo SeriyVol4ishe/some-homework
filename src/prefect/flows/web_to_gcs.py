5e7f11f93adbbf8958379924f4cda4dd46928937
seriyvol4ishe <sergii.d@helium10.com>
2023-02-27 16:04:08 +0200

    update some scripts ¯\_(ツ)_/¯;

--- src/prefect/flows/web_to_gcs.py
+++ src/prefect/flows/web_to_gcs.py
@@ -8,21 +8,22 @@ from settings import DATASETS_LOCAL_DIR_PATH
 
 
 @flow(log_prints=True)
-def web_to_gcs(color: str = "green", year: int = 2020, month: int = 1, remove_missing=True) -> None:
-    dataset_file_name = f"{color}_tripdata_{year}-{month:02}"
+def web_to_gcs(service: str = "green", year: int = 2020, month: int = 1, remove_missing=True) -> None:
+    print(f'Start task for {service} taxi, {year} year, {month} month')
+    dataset_file_name = f"{service}_tripdata_{year}-{month:02}"
     dataset_file_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/" \
-                       f"{color}/{dataset_file_name}.csv.gz"
+                       f"{service}/{dataset_file_name}.csv.gz"
 
     raw_dataset = extract_data(
         dataset_file_url=dataset_file_url
     )
-    dataset = transform_data(
-        raw_dataset=raw_dataset,
-        remove_missing=remove_missing,
-    )
+    dataset_filepath_local = Path(f"{DATASETS_LOCAL_DIR_PATH}/{service}_taxi/{dataset_file_name}.csv")
+    dataset_filepath_local.parent.mkdir(parents=True, exist_ok=True)
+    raw_dataset.to_csv(dataset_filepath_local, index=False)
+    dataset = transform_data(df=raw_dataset, remove_missing=remove_missing)
     load_data(
         dataset=dataset,
-        dataset_folder=f"{color}_taxi",
+        dataset_folder=f"{service}_taxi",
         dataset_file_name=dataset_file_name
     )
 
@@ -30,7 +31,8 @@ def web_to_gcs(color: str = "green", year: int = 2020, month: int = 1, remove_mi
 @task()
 def extract_data(dataset_file_url: str) -> pd.DataFrame():
     raw_dataset = pd.read_csv(
-        filepath_or_buffer=dataset_file_url
+        filepath_or_buffer=dataset_file_url,
+        low_memory=False,
     )
     print(f"Detected {len(raw_dataset)} records in dataset")
 
@@ -38,24 +40,52 @@ def extract_data(dataset_file_url: str) -> pd.DataFrame():
 
 
 @task()
-def transform_data(raw_dataset: pd.DataFrame, remove_missing: bool) -> pd.DataFrame:
-    for column_name in raw_dataset.columns:
-        if 'datetime' in column_name:
-            raw_dataset[column_name] = pd.to_datetime(raw_dataset[column_name])
+def transform_data(df: pd.DataFrame, remove_missing: bool) -> pd.DataFrame:
+    df.columns = df.columns.str.lower()
+    for column in df.columns:
+        if 'datetime' in column:
+            df[column] = pd.to_datetime(df[column])
+        if column.endswith('id'):
+            df[column] = df[column].astype(pd.Int64Dtype())
+        if column.endswith('_type'):
+            df[column] = df[column].astype(pd.Int64Dtype())
+        if column.endswith('_flag'):
+            applier = {
+                'Y': True,
+                'y': True,
+                'yes': True,
+                'Yes': True,
+                'YES': True,
+                'N': False,
+                'n': False,
+                'no': False,
+                'No': False,
+                'NO': False,
+            }
+            df[column] = df[column].map(applier).astype(pd.BooleanDtype())
+        if column.endswith('_count'):
+            df[column] = df[column].astype(pd.Int64Dtype())
     if remove_missing:
-        raw_dataset['passenger_count'] = raw_dataset['passenger_count'].fillna(0)
+        df['passenger_count'] = df['passenger_count'].fillna(0)
 
-    return raw_dataset
+    return df
 
 
 @task()
 def load_data(dataset: pd.DataFrame, dataset_folder: str, dataset_file_name: str) -> None:
     dataset_filepath_local = Path(f"{DATASETS_LOCAL_DIR_PATH}/{dataset_folder}/{dataset_file_name}.parquet")
     dataset_filepath_gcs = Path(f"{dataset_folder}/{dataset_file_name}.parquet")
+    # dataset_filepath_local = Path(f"{DATASETS_LOCAL_DIR_PATH}/{dataset_folder}/{dataset_file_name}.csv")
+    # dataset_filepath_gcs = Path(f"{dataset_folder}/{dataset_file_name}.csv")
 
     dataset_filepath_local.parent.mkdir(parents=True, exist_ok=True)
+    # dataset.to_csv(
+    #     dataset_filepath_local,
+    #     index=False,
+    # )
     dataset.to_parquet(
         dataset_filepath_local,
+        engine='pyarrow',
         compression='gzip'
     )
 
@@ -68,13 +98,13 @@ def load_data(dataset: pd.DataFrame, dataset_folder: str, dataset_file_name: str
 
 
 if __name__ == '__main__':
-    for year in [2019, 2020]:
-        for color in ['yellow', 'green', 'fhv']:
-            if color == 'fhv' and year != 2019:
-                break
-            for month in range(1, 13):
+    for month in range(1, 13):
+        for service in ['yellow', 'green', 'fhv']:
+            for year in [2019, 2020]:
+                if service == 'fhv' and year != 2019:
+                    break
                 web_to_gcs(
-                    color=color,
+                    service=service,
                     year=year,
                     month=month,
                     remove_missing=False,
